567b010d92b61dfaf2e9279739e11b6632df87c1
Heniker <heniker@mail.ru>
2023-07-06 23:17:09 +0300

    .

--- src/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Otp'
-export * from './PersistentStorage'
-export * from './Security'
-
-// yeah, let's not
-// const ctx = require.context('.', false, /^[^_].*(?!\.test\.)\.ts/)
-// export ctx.keys().map((key) => console.log(key))
